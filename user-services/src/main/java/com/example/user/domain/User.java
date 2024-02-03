2d0232752184026f6265aa0d1e48929ac4cb8b6b
ThanhKien00 <kien.nt112002@gmail.com>
2024-02-03 10:49:26 +0700

    commit code

---
+++ user-services/src/main/java/com/example/user/domain/User.java
@@ -0,0 +1,19 @@
+package com.example.user.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String username;
+
+}
