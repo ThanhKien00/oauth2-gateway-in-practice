3be95c54ec5eb0edada932384fc48dd74ebc3fe3
ThanhKien00 <kien.nt112002@gmail.com>
2024-02-04 21:01:03 +0700

    commit code

--- api-gateway/src/main/java/com/example/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/example/gateway/config/SecurityConfig.java
@@ -1,17 +1,10 @@
 package com.example.gateway.config;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2ClientProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
-import org.springframework.security.oauth2.client.web.server.WebSessionServerOAuth2AuthorizedClientRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 /**
@@ -23,40 +16,11 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity,
-                                                         OAuth2ClientProperties oAuth2ClientProperties){
-        httpSecurity.authorizeExchange(auth -> auth
-                        .anyExchange().authenticated())
-                .formLogin(Customizer.withDefaults());
-        if (!oAuth2ClientProperties.getRegistration().isEmpty()) {
-            httpSecurity.oauth2Login(login -> login.authorizedClientRepository(serverOAuth2AuthorizedClientRepository()))
-                    .oauth2ResourceServer(resourceServer -> resourceServer.jwt(Customizer.withDefaults()));
-        }
-        return httpSecurity.build();
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity serverHttp) {
+        serverHttp.authorizeExchange(exchanges -> exchanges.pathMatchers("/api/v1/**").authenticated())
+                .oauth2ResourceServer(resourceServer -> resourceServer.jwt(Customizer.withDefaults()));
+        serverHttp.csrf(ServerHttpSecurity.CsrfSpec::disable);
+        return serverHttp.build();
     }
 
-    @Bean
-    public ServerOAuth2AuthorizedClientRepository serverOAuth2AuthorizedClientRepository() {
-        return new WebSessionServerOAuth2AuthorizedClientRepository();
-    }
-
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-    @Bean
-    MapReactiveUserDetailsService inMemoryUserDetailsManager() {
-        var user = User.builder()
-                .username("user")
-                .password("{noop}password")
-                .roles("USER")
-                .build();
-        var admin = User.builder()
-                .username("admin")
-                .password("{noop}password")
-                .roles("USER", "ADMIN")
-                .build();
-        return new MapReactiveUserDetailsService(user, admin);
-    }
 }
